a1276a6c87ac50193f4450846e3d940be68a0f65
MAAghakabiri <a.aghakabiri@gmail.com>
2022-10-14 16:40:29 +0330

    Add project files.

---
+++ MBI Body/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MBI_Body
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBMI = new System.Windows.Forms.Button();
+            this.tbWeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbGhad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBMI
+            // 
+            this.btnBMI.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBMI.Location = new System.Drawing.Point(85, 88);
+            this.btnBMI.Name = "btnBMI";
+            this.btnBMI.Size = new System.Drawing.Size(75, 32);
+            this.btnBMI.TabIndex = 0;
+            this.btnBMI.Text = "BMI";
+            this.btnBMI.UseVisualStyleBackColor = true;
+            this.btnBMI.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbWeight
+            // 
+            this.tbWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbWeight.Location = new System.Drawing.Point(59, 39);
+            this.tbWeight.Name = "tbWeight";
+            this.tbWeight.Size = new System.Drawing.Size(127, 31);
+            this.tbWeight.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "قد :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "وزن :";
+            // 
+            // tbGhad
+            // 
+            this.tbGhad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbGhad.Location = new System.Drawing.Point(59, 2);
+            this.tbGhad.Name = "tbGhad";
+            this.tbGhad.Size = new System.Drawing.Size(127, 31);
+            this.tbGhad.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 198);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbGhad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbWeight);
+            this.Controls.Add(this.btnBMI);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBMI;
+        private System.Windows.Forms.TextBox tbWeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbGhad;
+    }
+}
+
---
+++ MBI Body/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MBI_Body
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double mbi = (Convert.ToInt32(tbGhad.Text) - 100) - ((Convert.ToInt32(tbGhad.Text) - 150) / 4);
+            if (mbi > Convert.ToInt32(tbWeight.Text))
+                MessageBox.Show(" کاهش وزن هستید "+" kg " +" شما دارای " + (mbi - Convert.ToDouble(tbWeight.Text)) , "وزن" , MessageBoxButtons.OK , MessageBoxIcon.Information);
+            else if (mbi < Convert.ToInt32(tbWeight.Text))
+                MessageBox.Show(" اضافه وزن هستید "+ " kg " + " شما دارای " + (Convert.ToDouble(tbWeight.Text) - mbi) , "وزن", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("شما دارای وزن مناسب هستید", "وزن", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
